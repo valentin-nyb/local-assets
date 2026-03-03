3f94ddd5b080edfd3ac35292024ae6089cb045e4
valentin-nyb <valentin@notyourbrew.com>
2026-03-03 22:43:33 +0000

    fix(upload): Set multipart threshold to 5MB and relax CORS headers

--- worker-proxy.js
+++ worker-proxy.js
@@ -1,7 +1,7 @@
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, PUT, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+  "Access-Control-Allow-Headers": "*",
   "Access-Control-Max-Age": "86400",
 };
 
