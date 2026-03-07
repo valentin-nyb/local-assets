359975403ce84c393093189185b6574eb36b18c9
valentin-nyb <valentin@notyourbrew.com>
2026-03-07 15:52:09 +0000

    update

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-require ('dotenv').config();
 
-import MUX from '@mux/mux-node';
+import 'dotenv/config';
+import Mux from '@mux/mux-node';
 
-const MUX tokenId = process.env.MUX_TOKEN_ID;
-const MUX tokenSecret = process.env.MUX_TOKEN_SECRET;
+const muxTokenId = process.env.MUX_TOKEN_ID;
+const muxTokenSecret = process.env.MUX_TOKEN_SECRET;
 
-await Mux.video.uploads.create({
-  new_asset_settings: {
-    playback_policy: ['public'],
-    mp4_support: 'standard'
-  },
-  cors_origin: '*',
-  webhook_url: 'https://local-assets.com/assets'
-}).then((upload) => {
-  console.log('Upload URL:', upload.url);
+const mux = new Mux(muxTokenId, muxTokenSecret);
+
+async function createUpload() {
+  try {
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        playback_policy: ['public'],
+        mp4_support: 'standard'
+      },
+      cors_origin: '*',
+      webhook_url: 'https://local-assets.com/assets'
+    });
+    console.log('Upload URL:', upload.url);
+  } catch (err) {
+    console.error('Error creating upload:', err);
+  }
 }
+
+createUpload();
