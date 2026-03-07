4f0271d15a0a414ba457b1f18f45bdd931823a91
valentin-nyb <valentin@notyourbrew.com>
2026-03-07 14:18:19 +0000

    updatewebhook

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require ('dotenv').config();
 
 import MUX from '@mux/mux-node';
 
-const MUX tokenId = process.env.49c2dd4c-5ba5-4e93-b60b-54afee697b41;
-const MUX tokenSecret = process.env.M3W9cB0gXQ2ki+nYpJc9pdWYW5q0ZlCORrd8URup4lqlUyxbiRfXssWRNdWRiQbEmIZkPSgIjTT;
+const MUX tokenId = process.env.MUX_TOKEN_ID;
+const MUX tokenSecret = process.env.MUX_TOKEN_SECRET;
 
 await Mux.video.uploads.create({
   new_asset_settings: {
@@ -11,7 +11,7 @@ await Mux.video.uploads.create({
     mp4_support: 'standard'
   },
   cors_origin: '*',
-  webhook_url: 'https://local-assets.com'
+  webhook_url: 'https://local-assets.com/assets'
 }).then((upload) => {
   console.log('Upload URL:', upload.url);
 }
