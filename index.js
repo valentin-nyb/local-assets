d9095c52328e4175662ea46644f11912bfbdeb7f
valentin-nyb <valentin@notyourbrew.com>
2026-03-07 13:53:51 +0000

    update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require ('dotenv').config();
 
 import MUX from '@mux/mux-node';
 
-const MUX tokenId = process.env.MUX_TOKEN_ID;
-const MUX tokenSecret = process.env.MUX_TOKEN_SECRET;
+const MUX tokenId = process.env.49c2dd4c-5ba5-4e93-b60b-54afee697b41;
+const MUX tokenSecret = process.env.M3W9cB0gXQ2ki+nYpJc9pdWYW5q0ZlCORrd8URup4lqlUyxbiRfXssWRNdWRiQbEmIZkPSgIjTT;
 
 await Mux.video.uploads.create({
   new_asset_settings: {
