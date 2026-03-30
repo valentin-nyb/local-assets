786e460a80a30e2828b14dbadd14dcf938d914f8
valentin-nyb <valentin@notyourbrew.com>
2026-03-30 19:19:27 -0300

    update list-assets.js

---
+++ api/list-assets.js
@@ -0,0 +1,16 @@
+import Mux from '@mux/mux-node';
+
+const mux = new Mux({
+  tokenId: process.env.PROD_MUX_TOKEN_ID,
+  tokenSecret: process.env.PROD_MUX_TOKEN_SECRET
+});
+
+export default async function handler(req, res) {
+  try {
+    // Fetches the 25 most recent videos from your Mux account
+    const assets = await mux.video.assets.list({ limit: 25 });
+    return res.status(200).json(assets);
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
+}
