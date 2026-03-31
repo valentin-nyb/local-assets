32fda3e96610fcdc836d08d030435e474a097388
valentin-nyb <valentin@notyourbrew.com>
2026-03-31 15:43:45 -0300

    updated data.url

--- api/list-assets.js
+++ api/list-assets.js
@@ -7,9 +7,8 @@ const mux = new Mux({
 
 export default async function handler(req, res) {
   try {
-    const response = await mux.video.assets.list({ limit: 25 });
-    // Extract the plain array from the SDK response object
-    const assets = response.data ?? response;
+    // Fetches the 25 most recent videos from your Mux account
+    const assets = await mux.video.assets.list({ limit: 25 });
     return res.status(200).json(assets);
   } catch (e) {
     return res.status(500).json({ error: e.message });
