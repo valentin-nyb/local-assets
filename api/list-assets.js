a7dcf1a1d0a113fabccde63fc1162845d82fdad5
valentin-nyb <valentin@notyourbrew.com>
2026-04-01 11:15:03 -0300

    update

--- api/list-assets.js
+++ api/list-assets.js
@@ -1,11 +1,11 @@
 import Mux from '@mux/mux-node';
 
-const Mux = require('@mux/mux-node').default;
-  tokenId: process.env.PROD_MUX_TOKEN_ID,
-  tokenSecret: process.env.PROD_MUX_TOKEN_SECRET
+const mux = new Mux({
+  tokenId: process.env.PROD_MUX_TOKEN_ID || process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.PROD_MUX_TOKEN_SECRET || process.env.MUX_TOKEN_SECRET
 });
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   try {
     // Fetches the 25 most recent videos from your Mux account
     const assets = await mux.video.assets.list({ limit: 25 });
