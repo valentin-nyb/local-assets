fd06e9da123468db945379187504826d97bca2e1
valentin-nyb <valentin@notyourbrew.com>
2026-04-06 19:03:40 -0300

    updated total assets downloaded

--- api/list-assets.js
+++ api/list-assets.js
@@ -7,8 +7,8 @@ const mux = new Mux({
 
 export default async function handler(req, res) {
   try {
-    // Fetches the 25 most recent videos from your Mux account
-    const assets = await mux.video.assets.list({ limit: 25 });
+    // Fetches up to 100 assets from your Mux account
+    const assets = await mux.video.assets.list({ limit: 100 });
     return res.status(200).json(assets);
   } catch (e) {
     return res.status(500).json({ error: e.message });
